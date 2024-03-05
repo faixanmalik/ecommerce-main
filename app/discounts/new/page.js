1bec2e97d80cbdaad355563a24a8c946f3c79546
faixanmalik <faixan.malik554@gmail.com>
2024-03-05 12:11:28 +0500

    h 28 set

--- app/discounts/new/page.js
+++ app/discounts/new/page.js
@@ -1430,7 +1430,7 @@ export default function NewDiscount() {
                   leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                   leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
-                  <Dialog.Panel className="w-full h-screen mt-14 md:h-96 relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:w-full sm:max-w-lg">
+                  <Dialog.Panel className="w-full h-[31rem] mt-14 md:h-96 relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:w-full sm:max-w-lg">
                     <div className="bg-white sm:px-0 sm:pb-4">
                       <div className="sm:items-start w-full">
                         <div className="text-center mt-0 sm:text-left">
@@ -1452,7 +1452,7 @@ export default function NewDiscount() {
                             <input name="searchProduct" value={searchProduct} onChange={handleSearch} type="search" id="default-search" className="block w-full py-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 placeholder-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Search products...' required />
                           </div>
 
-                          <div className="h-full md:h-[18rem] overflow-y-scroll">
+                          <div className="h-[28rem] md:h-[18rem] overflow-y-scroll">
                             {dbProducts.map((item, index) => {
 
                               return <div key={index} className="flex justify-between items-center border-t border-b py-2 text-sm text-gray-700">
