7dc8a88c6537c7b9a26c7ce3c2dd4d000392fa6f
faixanmalik <faixan.malik554@gmail.com>
2024-03-13 22:14:00 +0500

    update

--- app/discounts/page.js
+++ app/discounts/page.js
@@ -141,7 +141,7 @@ const DiscountsPage = () => {
           alt="Discounts Image"
         />
         <Title>Manage discounts and promotions</Title>
-        <Text className="text-center pb-4 w-10/12">
+        <Text className="text-center pb-4 w-11/12">
           Create discount codes and automatic discounts that apply at checkout.
           You can also use discounts with compare at prices.
         </Text>
@@ -153,7 +153,7 @@ const DiscountsPage = () => {
 
         <div className="flex justify-between items-center px-4 md:px-2 py-2">
 
-          <div className="w-[70%] overflow-x-scroll border-r-2 border-gray-300 sm:border-none">
+          <div className="w-[60%] overflow-x-scroll border-r-2 border-gray-300 sm:border-none">
             <div className="flex items-center space-x-4 md:space-x-4">
               {tableButtons.map((item, index)=>{
                 return <button key={index} onClick={()=>setButtonIndex(index)} className={`font-semibold text-sm md:text-sm px-3 py-1 ${buttonIndex === index ? 'bg-gray-200 rounded-md' : ''}`}>{item.name}</button>
